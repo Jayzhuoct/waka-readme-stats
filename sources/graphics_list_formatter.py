50dab4ba62a8d627688820ed9090cbc2074a4014
Dexter <74092531+Jayzhuoct@users.noreply.github.com>
2023-08-29 20:56:05 +0800

    Update graphics_list_formatter.py

--- sources/graphics_list_formatter.py
+++ sources/graphics_list_formatter.py
@@ -21,8 +21,7 @@ class Symbol(Enum):
 
     VERSION_1 = "█", "░"
     VERSION_2 = "⣿", "⣀"
-    VERSION_3 = "⬛", "⬜"
-    VERSION_4 = "🟩", "⬜"
+    VERSION_3 = "🟩", "⬜"
 
     @staticmethod
     def get_symbols(version: int) -> Tuple[str, str]:
